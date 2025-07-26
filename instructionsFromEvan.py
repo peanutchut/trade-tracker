d3f796c9053a1aa31f46a4d06a7f3e01f91a0c43
Evan Arumbaka <esarumbaka@gmail.com>
2025-07-25 22:48:18 -0400

    added instructions and stuff to stay organized and know where i put all my shut

---
+++ instructionsFromEvan.py
@@ -0,0 +1,15 @@
+'''
+These are the git commands i run to start the discord bot
+
+source /Users/evanarumbaka/Desktop/DISCORD_BOT/.venv/bin/activate
+pip install discord.py
+python /Users/evanarumbaka/Desktop/DISCORD_BOT/main.py
+
+# This script is used to run a Discord bot that listens for trade signals in a specific channel
+# and records them in a Google Sheet. It uses the discord.py library for Discord interactions
+# and gspread for Google Sheets API interactions.
+
+It should be the same files as the ones in github
+
+either way js practicing pushing pulling new things
+'''
